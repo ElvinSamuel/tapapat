9fc1374de0d8f69b45676eb5114788588709f3d1
ElvinSamuel <elvinsamuel1@gmail.com>
2018-05-04 21:50:00 +0000

    First commit -- thought I did already. I changed the 'app.set('view engine', __)' from 'public' to 'views' so that everything's pulling from the right directories.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require("express"),
-    app = require(express);
+    app = express();
+
+/*Have Express Look In The Public Folder For Views*/
+app.use(express.static("views"));
+
+app.set("view engine", "ejs");
+
     
 app.get("/", function(req, res){
-    
-    res.render("circlesClean.html");
+    res.render("index");
 });
 
+
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Listening On Port" + process.env.PORT);
+    console.log("Listening On Port " + process.env.PORT);
 });
